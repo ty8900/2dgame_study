c035aab7128322f9eb7e32a3a26dcee7dedf0767
ty8900 <ty8900@naver.com>
2021-01-05 21:55:59 +0900

    chapter 5-6

---
+++ Assets/Scripts/Monobehaviours/Player.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    public HealthBar healthBarPrefab;
+    HealthBar healthbar;
+
+    public Inventory inventoryPrefab;
+    Inventory inventory;
+
+    public void Start()
+    {
+        hitPoints.value = startingHitPoints;
+
+        healthbar = Instantiate(healthBarPrefab);
+        inventory = Instantiate(inventoryPrefab);
+        healthbar.character = this;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("CanBePickedUp"))
+        {
+            Item hitObject = collision.gameObject.GetComponent<Consumable>().item;
+
+            if (hitObject != null)
+            {
+                bool shouldDisappear = false;
+                switch (hitObject.itemType)
+                {
+                    case Item.ItemType.COIN:
+                        shouldDisappear = inventory.AddItem(hitObject);
+                        break;
+                    case Item.ItemType.HEALTH:
+                        shouldDisappear = AdjustHitPoints(hitObject.quantity);
+                        break;
+                    default:
+                        break;
+                }
+
+                if (shouldDisappear)
+                collision.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public bool AdjustHitPoints(int amount)
+    {
+        if (hitPoints.value < maxHitPoints)
+        {
+            hitPoints.value = hitPoints.value + amount;
+
+            print("Adjusted hitPoints by: " + amount + ". New Value: " + hitPoints.value);
+
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ Assets/Scripts/Scriptable Objects/HealthBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public HitPoints hitPoints;
+
+    [HideInInspector]
+    public Player character;
+
+    public Image meterImage;
+    public Text hpText;
+
+    float maxHitpoints;
+
+    void Start()
+    {
+        maxHitpoints = character.maxHitPoints;
+    }
+
+    void Update()
+    {
+        if (character != null)
+        {
+
+            meterImage.fillAmount = hitPoints.value / maxHitpoints;
+
+            hpText.text = "HP: " + (meterImage.fillAmount * 100);
+        }
+    }
+}
